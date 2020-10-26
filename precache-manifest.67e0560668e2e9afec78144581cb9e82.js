6f52ef54c514aa54ed2b363b6a839f434a81fd2d
KirillLutsenko <mnrovski68@gmail.com>
2020-10-26 15:31:23 +0200

    Updates

---
+++ precache-manifest.67e0560668e2e9afec78144581cb9e82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d5ee50f815bb17d64026d24189522f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "89d7331084ffa61f9b2b",
+    "url": "./static/css/main.0aa3b6e8.chunk.css"
+  },
+  {
+    "revision": "dc54a6ac761ceda0f38e",
+    "url": "./static/js/2.e3c77272.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e3c77272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89d7331084ffa61f9b2b",
+    "url": "./static/js/main.a408812f.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
